7a57e49ba337d5cf7fda38062841bab12ede744c
shamshams <shamila.remtoula@hotmail.fr>
2021-03-28 16:24:01 +0200

    add api data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,30 @@
 import * as React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 
 import { Layout, SEO } from "../components";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO />
-  </Layout>
-);
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allCharacter(limit: 5, filter: { occupation: { eq: "Student" } }) {
+        nodes {
+          id
+          name
+          occupation
+          alias
+          images
+          quirk
+        }
+      }
+    }
+  `);
+  console.log(data.allCharacter.nodes);
+
+  return (
+    <Layout>
+      <SEO />
+    </Layout>
+  );
+};
 
 export default IndexPage;
